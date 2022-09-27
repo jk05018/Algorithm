578f495180ebb6ac76b3c01a0bab39ea7a53c44d
SeungHan <jk05018@naver.com>
2022-09-27 18:46:33 +0900

    Today Leetcode Problem
    p17. LetterCombination
    p22. GenerateParenthesis

---
+++ src/main/java/com/example/algorithm/leetcode/backtracking/GenerateParenthesis22.java
@@ -0,0 +1,26 @@
+package com.example.algorithm.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis22 {
+	static List<String> answer;
+
+	public List<String> generateParenthesis(int n) {
+		answer = new ArrayList<>();
+		solve(0,0,"",n);
+
+		return answer;
+	}
+
+	public void solve(int left, int right, String ans, int n){
+		if(left < right || left > n) return;
+		if(left == n && right == n){
+			answer.add(ans);
+			return;
+		}
+
+		solve(left + 1, right, ans + "(",n);
+		solve(left , right + 1, ans + ")",n);
+	}
+}

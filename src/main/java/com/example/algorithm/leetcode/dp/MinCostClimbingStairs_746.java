2bd09e800d02b4bd1b69a48e1e74df136a0440b9
SeungHan <jk05018@naver.com>
2022-10-05 17:23:47 +0900

    Today Jongmanbook dp
    ClimbingStairs
    MinCostClimbingStairs

---
+++ src/main/java/com/example/algorithm/leetcode/dp/MinCostClimbingStairs_746.java
@@ -0,0 +1,40 @@
+package com.example.algorithm.leetcode.dp;
+
+import java.util.*;
+
+public class MinCostClimbingStairs_746 {
+	static final int INF = 987_654_321;
+	static int N;
+	static int[] cache;
+
+	public int minCostClimbingStairs(int[] cost) {
+		// init Cache
+		N = cost.length;
+		cache = new int[N];
+		Arrays.fill(cache, -1);
+
+		return Math.min(climb(cost, 0), climb(cost, 1));
+	}
+
+	private int climb(int[] cost, int start) {
+		// base case
+		if (start == N)
+			return 0;
+
+		// check Cache
+		if (cache[start] != -1) {
+			return cache[start];
+		}
+
+		// solve
+		int ret = INF;
+
+		for (int next = 1; next <= 2; ++next) {
+			if (start + next <= N) {
+				ret = Math.min(ret, cost[start] + climb(cost, start + next));
+			}
+		}
+
+		return cache[start] = ret;
+	}
+}

5526b1bdfcd762e4d49cae1f83ad334ca077c2fa
SeungHan <jk05018@naver.com>
2022-10-10 21:42:43 +0900

    Today leetcode backtracking
    PathSum2

---
+++ src/main/java/com/example/algorithm/leetcode/backtracking/PathSum2_113.java
@@ -0,0 +1,62 @@
+package com.example.algorithm.leetcode.backtracking;
+
+import java.util.*;
+
+public class PathSum2_113 {
+	static List<List<Integer>> answers;
+
+	public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+		// init Settings
+		answers = new ArrayList<>();
+
+		solve(root, targetSum, new ArrayList<Integer>(), 0);
+
+		return answers;
+	}
+
+	public void solve(TreeNode root, int target, List<Integer> answer, int sum) {
+		// base case
+		if (root == null) {
+			return;
+		}
+
+		// if arrived at leaf node
+		if (root.left == null && root.right == null) {
+			if (sum + root.val == target) {
+				answer.add(root.val);
+				answers.add(new ArrayList<Integer>(answer));
+				answer.remove(answer.size() - 1);
+			}
+			return;
+		}
+
+		answer.add(root.val);
+		if (root.left != null) {
+			solve(root.left, target, answer, sum + root.val);
+		}
+		if (root.right != null) {
+			solve(root.right, target, answer, sum + root.val);
+		}
+		answer.remove(answer.size() - 1);
+
+	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}

2ed731407781f7533723f198faa3140027656483
SeungHan <jk05018@naver.com>
2022-10-15 20:31:49 +0900

    Today Leetcode backtracking
    CombinationSum3
    PalindromePartitioning
    WordBreak

---
+++ src/main/java/com/example/algorithm/leetcode/backtracking/CombinationSum3_216.java
@@ -0,0 +1,46 @@
+package com.example.algorithm.leetcode.backtracking;
+
+import java.util.*;
+
+public class CombinationSum3_216 {
+	static List<List<Integer>> answers;
+	static int N, K;
+
+	public List<List<Integer>> combinationSum3(int k, int n) {
+		// init Settings
+		answers = new ArrayList<>();
+		K = k;
+		N = n;
+
+		find(new ArrayList<>(), new boolean[10], 1, 0);
+
+		return answers;
+	}
+
+	public void find(List<Integer> answer, boolean[] taken, int index, int sum) {
+		// base case
+		if (N < sum) {
+			return;
+		}
+
+		if (sum == N && answer.size() == K) {
+			answers.add(new ArrayList<Integer>(answer));
+			return;
+		}
+
+		// solve
+		for (int i = index; i < 10; ++i) {
+			if (taken[i])
+				continue;
+
+			answer.add(i);
+			taken[i] = true;
+
+			find(answer, taken, i + 1, sum + i);
+
+			answer.remove(answer.size() - 1);
+			taken[i] = false;
+
+		}
+	}
+}
